ff1d016f61db731ead627b511e930375e2033db4
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-04-05 15:12:47 +0900

    feat: 02 이벤트 핸들러 등록 및 bind

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
   element: HTMLFormElement;
+  // form
+  titleInputEl: HTMLInputElement;
+  descInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = document.getElementById(
@@ -9,12 +13,38 @@ class ProjectInput {
     )! as HTMLTemplateElement;
     this.hostEl = document.getElementById("app")! as HTMLDivElement;
 
+    // form element
     const importedNode = document.importNode(this.templateEl.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputEl = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descInputEl = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputEl = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    // event
+    this.configure();
+
+    // rendering
     this.attatch();
   }
 
-  attatch() {
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log("this", this.titleInputEl.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  private attatch() {
     this.hostEl.insertAdjacentElement("afterbegin", this.element);
   }
 }
