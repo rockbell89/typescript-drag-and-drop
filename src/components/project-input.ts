52cffd78c159e73b23d14cd82961e078f0907dfb
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-04-08 02:50:50 +0900

    feat: 13 webpack 설정

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import { AutoBind } from "../decorators/autobind.js";
-import { projectState } from "../state/project-state.js";
-import { Validatable, validate } from "../utils/validation.js";
-import { Component } from "./base-component.js";
+import { AutoBind } from "../decorators/autobind";
+import { projectState } from "../state/project-state";
+import { Validatable, validate } from "../utils/validation";
+import { Component } from "./base-component";
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   // form
--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { AutoBind } from "../decorators/autobind.js";
-import { Draggable } from "../models/drag-and-drop.js";
-import { Project } from "../models/project.js";
-import { Component } from "./base-component.js";
+import { AutoBind } from "../decorators/autobind";
+import { Draggable } from "../models/drag-and-drop";
+import { Project } from "../models/project";
+import { Component } from "./base-component";
 
 export class ProjectItem
   extends Component<HTMLUListElement, HTMLLIElement>
